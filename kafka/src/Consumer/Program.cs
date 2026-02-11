f6b4427a6e181a3e81e2622d3f17057b890302fe
dncsvr <disever10@alm.ku.edu.tr>
2026-02-12 01:50:12 +0300

    add sample Consumer and Producer
    - add compose file for kafka

---
+++ kafka/src/Consumer/Program.cs
@@ -0,0 +1,22 @@
+﻿using Confluent.Kafka;
+
+using var consumer = new ConsumerBuilder<string, string>(new ConsumerConfig
+{
+    BootstrapServers = "localhost:9092",
+    GroupId = "1",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+}).Build();
+
+consumer.Subscribe("demo-topic");
+
+while (true)
+{
+    var result = consumer.Consume(TimeSpan.FromSeconds(5));
+    if (result == null)
+    {
+        Console.WriteLine("Awaiting Message");
+        continue;
+    }
+
+    Console.WriteLine($"Key: {result.Message.Key}, Value: {result.Message.Value}, Offset: {result.Offset}");
+}
---
+++ kafka/src/Producer/Program.cs
@@ -0,0 +1,27 @@
+﻿using Confluent.Kafka;
+
+using var producer = new ProducerBuilder<string, string>(new ProducerConfig()
+{
+    BootstrapServers = "localhost:9092",
+    Acks = Acks.All,
+}).Build();
+
+while (true)
+{
+    try
+    {
+        var result = await producer.ProduceAsync("demo-topic", new Message<string, string>
+        {
+            Key = Guid.NewGuid().ToString(),
+            Value = $"Message #{DateTime.Now:yyyyMMddhhmmss}"
+        });
+
+        Console.WriteLine($"Key:{result.Message.Key}, Value: {result.Message.Value}, Offset: {result.Offset}");
+
+        await Task.Delay(2000);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+}
