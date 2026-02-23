47a60a8e839eb908968f11723c2b39e1be20457b
dncsvr <disever10@alm.ku.edu.tr>
2026-02-23 23:24:10 +0300

    improve kafka sample consumers
    - move use cases to their own files

---
+++ kafka/src/Consumer/BatchConsumer.cs
@@ -0,0 +1,57 @@
+﻿using Confluent.Kafka;
+using System.Diagnostics;
+
+namespace Consumer;
+
+public class BatchConsumer
+{
+    public async Task RunAsync()
+    {
+        var config = new ConsumerConfig
+        {
+            BootstrapServers = "localhost:9092",
+            GroupId = "12",
+            AutoOffsetReset = AutoOffsetReset.Earliest,
+            EnableAutoCommit = false
+        };
+        var consumerBuilder = new ConsumerBuilder<string, string>(config);
+
+        using var consumer = consumerBuilder.Build();
+        consumer.Subscribe("demo-topic");
+
+        var batch = new List<ConsumeResult<string, string>>();
+
+        var stopWatch = new Stopwatch();
+        stopWatch.Start();
+
+        while (true)
+        {
+
+            var result = consumer.Consume(TimeSpan.FromSeconds(5));
+            if (result == null) continue;
+
+            batch.Add(result);
+            if (batch.Count < 5 && stopWatch.ElapsedMilliseconds <= TimeSpan.FromSeconds(1).TotalMilliseconds)
+            {
+                continue;
+            }
+
+            foreach (var item in batch)
+            {
+                Console.WriteLine(
+                    $"Partition: {item.Partition.Value}," +
+                    $"Offset: {item.Offset}," +
+                    $"Key: {item.Message.Key}," +
+                    $"Value: {item.Message.Value}"
+                );
+            }
+
+            batch.Clear();
+            consumer.Commit();
+
+            Console.WriteLine("awating next batch");
+            await Task.Delay(1000);
+            stopWatch.Restart();
+        }
+    }
+}
--- kafka/src/Consumer/Program.cs
+++ kafka/src/Consumer/Program.cs
@@ -1,46 +1,10 @@
-﻿using Confluent.Kafka;
+﻿using Consumer;
 
-var config = new ConsumerConfig
-{
-    BootstrapServers = "localhost:9092",
-    GroupId = "1",
-    AutoOffsetReset = AutoOffsetReset.Earliest
-};
-var consumerBuilder = new ConsumerBuilder<string, string>(config);
-var consumerWithNoCommitBuilder = new ConsumerBuilder<string, string>(new ConsumerConfig(config) { EnableAutoCommit = false });
+//var multipleConsumers = new MultipleConsumers();
+//await multipleConsumers.RunAsync();
 
-Task[] consumers = [
-    Consume(consumerBuilder, 0),
-    Consume(consumerWithNoCommitBuilder, 1, manualCommit: true),
-    Consume(consumerWithNoCommitBuilder, 2)
-];
+var batchConsumer = new BatchConsumer();
+await batchConsumer.RunAsync();
 
-await Task.WhenAll(consumers);
-
-Task Consume(ConsumerBuilder<string, string> builder, int consumerId,
-    bool manualCommit = false
-) =>
-    Task.Run(() =>
-    {
-        using var consumer = builder.Build();
-        consumer.Subscribe("demo-topic");
-
-        while (true)
-        {
-            var result = consumer.Consume(TimeSpan.FromSeconds(5));
-            if (result == null) continue;
-
-            Console.WriteLine(
-                $"Consumer: {consumerId}," +
-                $"Partition: {result.Partition.Value}," +
-                $"Offset: {result.Offset}," +
-                $"Key: {result.Message.Key}," +
-                $"Value: {result.Message.Value}"
-            );
-
-            if (manualCommit)
-            {
-                consumer.Commit(result);
-            }
-        }
-    });
+//var exceedMaxPollIntervalConsumer = new ExceedMaxPollIntervalConsumer();
+//await exceedMaxPollIntervalConsumer.RunAsync();
