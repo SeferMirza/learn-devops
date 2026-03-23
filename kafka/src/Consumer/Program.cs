598fcac8c4306b6e6827bd4ccc040ce1e762b3eb
dncsvr <disever10@alm.ku.edu.tr>
2026-03-23 16:27:53 +0300

    minor edits

--- kafka/src/Consumer/Program.cs
+++ kafka/src/Consumer/Program.cs
@@ -1,12 +1,13 @@
 ﻿using Consumer;
 
-// /* Uncomment to use specific consumer setup */
+// Uncomment code below to use specific consumer setup
 // var multipleConsumers = new MultipleConsumers();
 // await multipleConsumers.RunAsync();
 
+// Uncomment code below to use specific consumer setup
 var batchConsumer = new BatchConsumer();
 await batchConsumer.RunAsync();
 
-// /* Uncomment to use specific consumer setup */
+// Uncomment code below to use specific consumer setup
 // var exceedMaxPollIntervalConsumer = new ExceedMaxPollIntervalConsumer();
 // await exceedMaxPollIntervalConsumer.RunAsync();
